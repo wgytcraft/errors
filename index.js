97b673c4753443d0f2c79cca8260baae671c1966
‍ <williamhorning@wgyt.tk>
2021-03-11 16:05:20 +0000

    fix

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ module.exports = function (host, res, req, errornumber, errordesc, version, ejs,
   res.writeHead(200, { 'Content-Type': 'text/html' });
   date = Date.now() * 1000
   date2 = new Date(date)
-  var data
-  var data.num = errornumber || config.errornumber
-  var data.desc = errordesc || config.errordesc
-  var data.ver = version || config.version
-  var data.date = date2.toLocaleString("en-US", { timeZoneName: "short", timeZone: "America/New_York" })
+  var datanum = errornumber || config.errornumber
+  var datadesc = errordesc || config.errordesc
+  var dataver = version || config.version
+  var datadate = date2.toLocaleString("en-US", { timeZoneName: "short", timeZone: "America/New_York" })
   console.log(`${errornumber} ${errordesc} error @ https://${host}${req.url}`)
   res.write(ejs.render(`<!doctype html>
 <html>
@@ -63,6 +62,6 @@ module.exports = function (host, res, req, errornumber, errordesc, version, ejs,
       </div>
     </div>
   </body>
-</html>`, { errornumber: errornumber, data.num: data.desc, url: `https://${host}${req.url}`, host: host || "wgytcraft selfhosted", timestamp: data.date, version: data.ver }));
+</html>`, { errornumber: errornumber, datanum: datadesc, url: `https://${host}${req.url}`, host: host || "wgytcraft selfhosted", timestamp: datadate, version: dataver }));
   res.end();
 }
